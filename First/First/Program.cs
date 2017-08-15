c96e1a749261bed00d63c2b7573d97c9dd482514
aes1998 <aou1998@gmail.com>
2017-08-16 00:52:28 +0300

    Fisrt my  C#
    :)

---
+++ First/First/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace First
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           //AES
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            int x = 5;
+            Console.Write("{0}",x );
+            Console.ReadKey();
+        }
+    }
+}
